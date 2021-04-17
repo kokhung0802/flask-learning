d7d8d539c8ae3e6dd933d7645ef634636f7ca2b8
kokhung0802 <kok1995hung@gmail.com>
2021-04-17 21:36:52 +0800

    C3 half

--- flaskblog.py
+++ flaskblog.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 # __name__ is a special variable, __name__ is __main__ if run with python directly
 app = Flask(__name__)
+
+# secrets.token_hex(16)
+app.config['SECRET_KEY'] = '663b3536f92f472dc9d27db401e05d46'
 
 posts = [{
     'author': 'ckh',
@@ -25,6 +29,18 @@ def hello():
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
